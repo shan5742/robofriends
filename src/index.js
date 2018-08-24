d219ca7a16f47572381644bd9c1c974290f91812
shan5742 <asamshan456@gmail.com>
2018-08-24 02:02:27 +0100

    add Card component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Card />, document.getElementById('root'));
 registerServiceWorker();
